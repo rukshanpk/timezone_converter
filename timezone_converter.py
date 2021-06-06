ed1e106a9c5683ffc12493ee7ba0ce5f673e11c0
Rukshan Karunarathne <rukshanpk@gmail.com>
2021-06-06 10:40:36 +0530

    commit 3 - merge the date and time together

--- timezone_converter.py
+++ timezone_converter.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 from datetime import date, datetime
 from pytz import timezone
 import sys
 
 
-def tz_converter(my_date, my_time, target_time_zone):
-    date_time_str = my_date+" "+my_time
+def tz_converter(my_date_time, target_time_zone):
+    date_time_str = my_date_time
     date_time_obj = datetime.strptime(
         date_time_str, '%Y-%m-%d %H:%M:%S')
     indian_standard = timezone('Asia/Colombo')
@@ -16,11 +17,10 @@ def tz_converter(my_date, my_time, target_time_zone):
     print(str(loc_dt)+" ----> "+str(target_dt))
 
 
-def main(start_date, start_time, end_date, end_time, target_time_zone):
-    tz_converter(sys.argv[1], sys.argv[2], sys.argv[5])
-    tz_converter(sys.argv[3], sys.argv[4], sys.argv[5])
+def main(start_date_time, end_date_time, target_time_zone):
+    tz_converter(sys.argv[1], sys.argv[3])
+    tz_converter(sys.argv[2], sys.argv[3])
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3],
-         sys.argv[4], sys.argv[5])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
